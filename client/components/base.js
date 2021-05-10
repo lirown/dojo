fee4772b47de4a3177928f3d6e42e345682843ae
Liron Goldenberg <lgoldenberg@forter.com>
2021-05-10 13:23:16 +0300

    fix: merge conflict

--- client/components/base.js
+++ client/components/base.js
@@ -1,4 +1,3 @@
 export { LitElement, html, css, svg } from 'lit';
 export { until } from 'lit/directives/until.js';
 export { asyncReplace } from 'lit/directives/async-replace.js';
-import { AsyncDirective } from 'lit/async-directive.js';
